73871d5f3d458539984bb1502bcba4d176e0ad6b
bekji <turayev07@gmail.com>
2021-04-01 14:59:27 +0500

    testing

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import App from './App';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+    shallow(<App />);
+});
